a9903371a090962af5c87853951a01ee7b416264
Anya <xxxuko@gmail.com>
2024-05-13 20:20:29 +0300

    create json writter

---
+++ app/writter/json_writter.py
@@ -0,0 +1,13 @@
+import json
+
+
+class JSONFileWriter:
+    def __init__(self, file_name: str) -> None:
+        self.file_name = file_name
+
+    def write_in_json_file(self, data: list) -> None:
+        serialized_data = [item.to_dict() for item in data]
+        with open(
+            self.file_name + ".json", "w", encoding="utf-8"
+        ) as file:
+            json.dump(serialized_data, file, indent=4)
