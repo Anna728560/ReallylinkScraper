7fd55af174ea21fda726060bdec50907113ab2bd
Anya <xxxuko@gmail.com>
2024-05-14 00:13:01 +0300

    add description for classes and replaced time.sleep with WebDriverWait in ItemScraper

--- app/scraper/scraper.py
+++ app/scraper/scraper.py
@@ -1,4 +1,5 @@
 import time
+from typing import List, Union, Dict
 
 from selenium import webdriver
 from selenium.common import NoSuchElementException
@@ -7,17 +8,26 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class WebScraperService:
-    BASE_URL = "https://realtylink.org/en/properties~for-rent?uc=0"
+from app.scraper.models import Item
 
+
+BASE_URL = "https://realtylink.org/en/properties~for-rent?uc=0"
+
+
+class PageScraper:
+    """
+    Class for scraping pages
+    """
     def __init__(self) -> None:
         self.options = Options()
         self.driver = webdriver.Chrome(options=self._add_options())
         self.action_chains = ActionChains(self.driver)
 
     def _add_options(self) -> Options:
-        # self.options.add_argument("--headless")
+        self.options.add_argument("--headless")
         self.options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -26,9 +36,12 @@ class WebScraperService:
 
         return self.options
 
-    def get_all_items(self):
-        self.driver.get(self.BASE_URL)
+    def get_all_items(self) -> List[str]:
+        self.driver.get(BASE_URL)
         time.sleep(2)
+        WebDriverWait(self.driver, 2).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "property-thumbnail-item"))
+        )
 
         links = []
 
@@ -42,22 +55,29 @@ class WebScraperService:
                 )
                 links.append(link_to_ad)
                 # if len(links) == 60:
-                if len(links) == 5:
+                if len(links) == 60:
                     return links
 
             self.click_next_page()
 
-    def click_next_page(self):
+    def click_next_page(self) -> None:
         pager = self.driver.find_element(By.CLASS_NAME, "pager")
         next_page_element = pager.find_element(By.CLASS_NAME, "next")
         if "inactive" not in next_page_element.get_attribute("class"):
-            time.sleep(1)
+            time.sleep(0.5)
             next_page_element.click()
-            time.sleep(1)
+            time.sleep(0.5)
 
-    def parse_single_item_by_link(self, link):
+
+class ItemScraper(PageScraper):
+    """
+    Class for scraping items
+    """
+    def parse_single_item_by_link(self, link: str) -> Item:
         self.driver.get(link)
-        time.sleep(2)
+        WebDriverWait(self.driver, 2).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'h1 [data-id="PageTitle"]'))
+        )
 
         address = self._get_address()
         region = address.split(",")[-1]
@@ -74,15 +94,18 @@ class WebScraperService:
             img_array=self._get_img_array(),
         )
 
-    def _get_title(self):
+    def _get_title(self) -> str:
         return self.driver.find_element(
             By.CSS_SELECTOR, 'h1 [data-id="PageTitle"]'
         ).text
 
-    def _get_address(self):
-        return self.driver.find_element(By.CSS_SELECTOR, 'h2[itemprop="address"]').text
+    def _get_address(self) -> str:
+        return self.driver.find_element(
+            By.CSS_SELECTOR,
+            'h2[itemprop="address"]'
+        ).text
 
-    def _get_price(self):
+    def _get_price(self) -> str:
         return (
             self.driver.find_element(
                 By.CSS_SELECTOR,
@@ -93,7 +116,7 @@ class WebScraperService:
             .split(" ")[0]
         )
 
-    def _get_description(self):
+    def _get_description(self) -> Union[str, None]:
         try:
             description = self.driver.find_element(
                 By.CSS_SELECTOR, "div.row.description-row > div > div:nth-child(2)"
@@ -102,7 +125,10 @@ class WebScraperService:
         except NoSuchElementException:
             return None
 
-    def _get_rooms(self):
+    def _get_rooms(self) -> Dict[str, int]:
+        """
+        Get the number of rooms
+        """
         room_elements = self.driver.find_elements(
             By.CSS_SELECTOR, "div.col-lg-12.description > div.row.teaser > div"
         )
@@ -118,13 +144,19 @@ class WebScraperService:
                     rooms["bathrooms"] = int(text.split()[0])
         return rooms
 
-    def _get_size_sqft(self):
+    def _get_size_sqft(self) -> str:
+        """
+        Get the size in square feet
+        """
         return self.driver.find_element(
             By.CSS_SELECTOR,
             "div.col-lg-12.description > div:nth-child(6) > div:nth-child(1) > div.carac-value > span",
         ).text
 
-    def _get_img_array(self):
+    def _get_img_array(self) -> List[str]:
+        """
+        Get the array of images
+        """
         first_image = self.driver.find_element(
             By.CSS_SELECTOR, "div.primary-photo-container > a"
         )
@@ -132,22 +164,19 @@ class WebScraperService:
 
         img_links = []
 
-        time.sleep(2)
+        WebDriverWait(self.driver, 2).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.description > strong"))
+        )
         description = self.driver.find_element(
             By.CSS_SELECTOR, "div.description > strong"
         ).text
         total_images = int(description.split("/")[1])
 
-        current_img = self.driver.find_element(
-            By.CSS_SELECTOR, 'img[src*="mediaserver.realtylink.org"]'
-        )
-        img_links.append(current_img.get_attribute("src"))
-
         while len(img_links) < total_images:
-            self.action_chains.send_keys(Keys.ARROW_RIGHT).perform()
             current_img = self.driver.find_element(
                 By.CSS_SELECTOR, 'img[src*="mediaserver.realtylink.org"]'
             )
             img_links.append(current_img.get_attribute("src"))
+            self.action_chains.send_keys(Keys.ARROW_RIGHT).perform()
 
         return img_links
